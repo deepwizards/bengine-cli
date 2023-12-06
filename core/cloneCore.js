8544d76b3e320839279f1d7ddbf082239a05720b
altninja <ben@grvs.io>
2023-12-06 12:40:40 +0700

    update install and update core

--- core/cloneCore.js
+++ core/cloneCore.js
@@ -1,17 +1,97 @@
 const { execSync } = require('child_process');
+const readlineSync = require('readline-sync'); // Ensure this dependency is installed
+const fs = require('fs');
+const path = require('path');
 
 module.exports = function cloneCore() {
-    console.log("Cloning bengine core repository...");
     try {
+        console.log("Cloning Bengine core repository...");
         execSync('git clone https://github.com/deepwizards/bengine.git', { stdio: 'inherit' });
+        process.chdir('./bengine');
         console.log("Repository cloned successfully.");
 
-        // Placeholder for additional installation processes
-        console.log("Running installation process...");
-        // TODO: Add installation steps here
+        console.log("Setting up the environment...");
 
-        console.log("Installation complete.");
+        // Admin account credentials setup
+        const adminUsername = readlineSync.question('Enter admin username: ');
+        const adminPassword = readlineSync.questionNewPassword('Enter admin password: ', { min: 8, max: 20 });
+
+        // DB connection setup
+        let dbConnection = readlineSync.question('Enter DB connection string or leave blank to use default containerized DB: ');
+        if (!dbConnection) {
+            // Logic to run a containerized DB
+            console.log("Setting up a default containerized database...");
+            execSync('docker-compose up -d database', { stdio: 'inherit' });
+            dbConnection = 'default DB connection string';
+        }
+        // Set DB URI in environment
+        process.env.BENGINE_DB_URI = dbConnection;
+        // Implement logic to test DB connection here
+
+        console.log("Seeding the database...");
+        // Determine which seeding script to run
+        if (readlineSync.keyInYN('Do you want to install the default demo project?')) {
+            execSync(`node main/seed/new_full.js ${adminUsername} ${adminPassword}`, { stdio: 'inherit' });
+        } else {
+            execSync('node main/seed/new_bare.js', { stdio: 'inherit' });
+        }
+
+        // Domain name setup
+        const domainName = readlineSync.question('Enter your domain name or leave blank for localhost: ');
+        if (domainName) {
+            // Logic for domain setup
+            // Update domain configuration in application settings
+        }
+
+        // Nginx / LetsEncrypt SSL setup
+        if (domainName && readlineSync.keyInYN('Do you want to set up Nginx and LetsEncrypt for SSL?')) {
+            console.log("Setting up Nginx and LetsEncrypt SSL...");
+            // Nginx configuration
+            const nginxConfig = `
+                server {
+                    listen 80;
+                    server_name ${domainName};
+
+                    location / {
+                        proxy_pass http://localhost:YOUR_APP_PORT; # Replace with your app's port
+                        proxy_http_version 1.1;
+                        proxy_set_header Upgrade \$http_upgrade;
+                        proxy_set_header Connection 'upgrade';
+                        proxy_set_header Host \$host;
+                        proxy_cache_bypass \$http_upgrade;
+                    }
+                }
+            `;
+
+            // Write Nginx config file
+            fs.writeFileSync(`/etc/nginx/sites-available/${domainName}`, nginxConfig);
+            execSync(`ln -s /etc/nginx/sites-available/${domainName} /etc/nginx/sites-enabled/`);
+
+            // Reload Nginx to apply new config
+            execSync('nginx -s reload');
+
+            // LetsEncrypt SSL setup
+            execSync(`certbot --nginx -d ${domainName} --non-interactive --agree-tos -m your-email@example.com`, { stdio: 'inherit' });
+
+            console.log("Nginx and SSL setup complete.");
+        }
+
+        console.log("Installing npm packages...");
+        execSync('npm install', { stdio: 'inherit' });
+
+        // Building service cluster
+        console.log("Building service cluster...");
+        execSync('cd services && docker-compose build', { stdio: 'inherit' });
+
+
+        console.log("Running post-install tests...");
+        // Logic for post-install tests
+        // Placeholder for now
+
+        console.log("Bengine core installation complete.");
+
     } catch (error) {
-        console.error("Failed to clone repository:", error);
+        console.error("Failed to install Bengine:", error);
+        console.log("Please check the errors above and try again.");
     }
 };
--- core/updateCore.js
+++ core/updateCore.js
@@ -1,16 +1,66 @@
 // core/updateCore.js
 const { execSync } = require('child_process');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = function updateCore() {
-    console.log("Updating the Bengine core...");
+    console.log("Checking for Bengine core updates...");
+
     try {
-        // Assuming the core is a Git repository
-        execSync('cd path/to/bengine/core && git pull', { stdio: 'inherit' });
+        // Determine the current working directory
+        const currentDir = process.cwd();
+        const bengineConfigFile = path.join(currentDir, 'bengine.json');
+
+        // Check if the bengine.json file exists
+        if (!fs.existsSync(bengineConfigFile)) {
+            throw new Error('No bengine.json file found. Are you in the right directory?');
+        }
+
+        // Read and parse bengine.json
+        const bengineConfig = JSON.parse(fs.readFileSync(bengineConfigFile, 'utf8'));
+        if (!bengineConfig.core) {
+            throw new Error('This directory does not seem to be a Bengine core installation.');
+        }
+
+        console.log("Found Bengine core installation. Updating...");
+
+        // Stop Bengine processes before updating
+        console.log("Stopping Bengine processes...");
+        execSync('bengine core stop', { stdio: 'inherit' });
+
+        // Fetch the latest changes from the repository
+        execSync('git fetch', { stdio: 'inherit' });
+
+        // Check if there are updates by comparing local and remote
+        const status = execSync('git status -uno').toString();
+        if (status.includes('Your branch is up to date')) {
+            console.log("Bengine core is already up to date.");
+
+            // Restart Bengine processes if no updates were found
+            console.log("Restarting Bengine processes...");
+            execSync('bengine core start', { stdio: 'inherit' });
+
+            return;
+        }
+
+        // Pull the updates
+        execSync('git pull', { stdio: 'inherit' });
+
+        // Install any new dependencies
+        execSync('npm install', { stdio: 'inherit' });
+
+        // Restart Bengine processes after updating
+        console.log("Restarting Bengine processes...");
+        execSync('bengine core start', { stdio: 'inherit' });
+
         console.log("Bengine core updated successfully.");
 
-        // Any additional steps for core update can be added here
-
     } catch (error) {
-        console.error("Failed to update the Bengine core:", error);
+        console.error("Failed to update the Bengine core. Error details:", error.message);
+        console.log("To resolve this issue:");
+        console.log("1. Make sure you are in the correct directory where the Bengine core is installed.");
+        console.log("2. Check your internet connection.");
+        console.log("3. Ensure you have the necessary permissions.");
+        console.log("4. Verify the Bengine core repository is not in a conflicted state.");
     }
 };
